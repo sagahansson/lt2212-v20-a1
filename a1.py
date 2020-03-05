8b70b171458f01ecb6380210689c61e22abf7c89
Saga Hansson <gushansad@GU.GU.SE@mltgpu.flov.gu.se>
2020-03-05 17:01:17 +0100

    fixed bonus

--- a1.py
+++ a1.py
@@ -5,8 +5,9 @@ import numpy as np
 import glob
 from collections import Counter
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+
 
 def get_tokenized_files(directory, directory2):
     """
